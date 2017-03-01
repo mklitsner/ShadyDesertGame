88aafbc991ed189d82d05733b1a4f363947d15ab
Miller Klitsner <mklitsner@gmail.com>
2017-02-28 22:20:14 -0800

    Add Player Controls and more variables to DesertWandererAI

--- Shade Manipulation Game/Assets/_Scripts/DesertWandererAI.cs
+++ Shade Manipulation Game/Assets/_Scripts/DesertWandererAI.cs
@@ -2,15 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class desertWanderer : MonoBehaviour {
+public class DesertWandererAI : MonoBehaviour {
+	bool inshade;
+	[Range(0, 1)]float heat;//0-1
+	[Range(0, 1)]float tiredness;//0-1
 
+
+
+	Vector3 sunPosition;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		DetectShade ();
+		SetHeat (0.001f);
+		Debug.Log (inshade);
 	}
-}
+
+
+
+
+	void DetectShade(){
+		sunPosition = GameObject.Find ("sunTarget").transform.position;
+		Ray ray = new Ray(transform.position, (sunPosition-transform.position));
+		RaycastHit hit;
+		//print (sunPosition - transform.position);
+		Debug.DrawRay (transform.position, (sunPosition - transform.position));
+
+		if (Physics.SphereCast (ray, 0.1f, out hit)) 
+		{
+			if (hit.transform.gameObject.name == "sunTarget") {
+				inshade = false;
+			} else {
+				inshade = true;
+			}
+		}
+	}
+
+
+	void SetState(int _state){
+
+		switch (_state) {
+		case 1:
+
+			break;
+
+		case 2:
+			
+			break;
+
+		case 3:
+			
+			break;
+
+		default:
+			break;
+		}
+		}
+
+	void SetHeat(float _deltaheat){
+		if(inshade){
+			if(heat<0){
+				heat = 0;
+			}else{
+				heat = heat-_deltaheat;
+				}
+			}
+		if(!inshade){
+			if(heat>1){
+				heat = 1;
+			}else{
+				heat = heat +_deltaheat;
+			}
+		}
+	}
+
+	void SetTiredness(float _deltatiredness){
+		if(heat>0.5){
+			if(tiredness>1){
+				tiredness = 1;
+			}else{
+				tiredness = tiredness+_deltatiredness;
+			}
+		}
+	}
+	}
+
+
+	
+
+
---
+++ Shade Manipulation Game/Assets/_Scripts/PlayerControls.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+public float sensitivity = 0.1f;
+
+
+	float sunXrotation=90;
+	//90=noon
+	float maxXrot=90;
+	//0=dusk
+	float minXrot=0;
+
+	float sunYrotation=0;
+
+	float sunXrotate;
+	float sunYrotate;
+	//global
+	//0=shade at 12oclock
+	//180= shade at 6oclock
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sunXrotation=transform.localEulerAngles.x;
+		sunYrotation=transform.eulerAngles.y;
+
+
+
+
+		ChangeSunCoordinates ();
+		MoveSun ();
+	}
+
+	void MoveSun(){
+		transform.Rotate (sunXrotate, 0, 0,Space.Self);
+		transform.Rotate (0, sunYrotate, 0,Space.World);
+	}
+
+
+
+
+	void ChangeSunCoordinates(){
+		if (Input.GetKey ("up")) {
+			print ("up arrow key is held down");
+			if (sunXrotation < maxXrot) {
+				sunXrotate = sensitivity;
+			}
+		} else if (Input.GetKey ("down")) {
+			print ("down arrow key is held down");
+			if (sunXrotation > minXrot) {
+				sunXrotate = -sensitivity;
+			}
+		} else {
+			sunXrotate = 0;
+		}
+
+		if (Input.GetKey("left")) {
+			print ("left arrow key is held down");
+			sunYrotate = sensitivity;
+		} else if (Input.GetKey ("right")) {
+			print ("right arrow key is held down");
+			sunYrotate = -sensitivity;
+		} else {
+			sunYrotate = 0;
+		}
+	}
+
+
+}
