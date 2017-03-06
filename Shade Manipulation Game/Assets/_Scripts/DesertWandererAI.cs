86a47cbe4ab28a0eec835892006cd71267c4469a
Miller Klitsner <mklitsner@gmail.com>
2017-03-06 03:33:59 -0800

    Add more to DesertWandererAI
    make a sensor for shadows in front of the wanderer, and add and edit
    functions in desert AI to mediate its status and states

--- Shade Manipulation Game/Assets/_Scripts/DesertWandererAI.cs
+++ Shade Manipulation Game/Assets/_Scripts/DesertWandererAI.cs
@@ -6,23 +6,79 @@ public class DesertWandererAI : MonoBehaviour {
 	bool inshade;
 	[Range(0, 1)]float heat;//0-1
 	[Range(0, 1)]float tiredness;//0-1
+	int state =wandering;
+	int asleep = 1;
+	int dead =2;
+	int wandering =3;
+	int sawSomething =4;
 
+	float speed;
 
-
-	Vector3 sunPosition;
+	public Vector3 sunPosition;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		float currentspeed;
+
+		currentspeed = speed - (tiredness * speed);
+
+		if (state == wandering) {
+		}
+
 		DetectShade ();
-		SetHeat (0.01f);
+		if (!inshade) {
+		SearchForShade ();
+		}
+		SetHeat (0.1f);
 		StateIndicator ();
 
 	}
 
 
+
+
+
+	void SetState(int _state){
+
+		switch (_state) {
+		case asleep:
+			//goes to sleep for a short amount of time
+			break;
+
+		case dead:
+			//dies and becomes cactus man
+			break;
+
+		case wandering:
+			//gets up and continues walking
+			break;
+
+		case sawSomething:
+			//perks up at the shade he saw
+			break;
+		}
+	}
+
+
+
+	void MoveForward(){
+		
+	}
+
+	void MoveRandomly(){
+	}
+
+	void SearchForShade(){
+		bool seeShade =transform.GetChild ().GetComponent<ShadeSearcher> ().inshade;
+
+		if (seeShade) {
+			SetState (sawSomething);
+
+		}
+	}
 
 
 	void DetectShade(){
@@ -43,52 +99,56 @@ public class DesertWandererAI : MonoBehaviour {
 	}
 
 
-	void SetState(int _state){
 
-		switch (_state) {
-		case 1:
 
-			break;
 
-		case 2:
-			
-			break;
 
-		case 3:
-			
-			break;
-
-		default:
-			break;
-		}
-		}
-
-	void SetHeat(float _deltaheat){
+	void SetHeat(float _increaseheat,float _decreaseheat){
 		if(inshade){
 			if(heat<0){
 				heat = 0;
 			}else{
-				heat = heat-_deltaheat;
+				heat = heat-_decreaseheat;
 				}
 			}
 		if(!inshade){
 			if(heat>1){
 				heat = 1;
 			}else{
-				heat = heat +_deltaheat;
+				heat = heat +_increaseheat;
 			}
 		}
 	}
+		
 
-	void SetTiredness(float _deltatiredness){
-		if(heat>0.5){
-			if(tiredness>1){
+	void SetTiredness(float _increasetiredness,float _decreasetiredness){
+		if (heat > 0.7 && state == wandering) {
+			if (tiredness > 1) {
 				tiredness = 1;
-			}else{
-				tiredness = tiredness+_deltatiredness;
+				if (inshade) {
+					SetState (asleep);
+				} else {
+					tiredness = tiredness + _increasetiredness;
+				}
 			}
 		}
+		if (state = asleep) {
+				if (inshade) {
+					if (tiredness < 0) {
+						tiredness = 0;
+						state = wandering;
+					} else {
+						tiredness = tiredness - _decreasetiredness;
+					}
+				} else {
+					if (heat => 1) {
+						SetState (dead);
+					}
+				}
+		}
 	}
+		
+
 
 
 	void StateIndicator(){
